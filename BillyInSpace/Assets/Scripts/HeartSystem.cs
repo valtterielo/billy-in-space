ef20319d6bcad1bd46620df11460f81c46805b77
valtterielo <valtteri.elo1@gmail.com>
2021-03-04 19:45:43 +0200

    initial commit

---
+++ BillyInSpace/Assets/Scripts/HeartSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartSystem : MonoBehaviour
+{
+    public GameObject[] hearts;
+    public int life;
+    private bool dead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        life = hearts.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dead == true)
+        {
+            //DIE
+        }
+    }
+
+    public void TakeDamage(int d)
+    {
+        life -= d;
+        Destroy(hearts[life].gameObject);
+        if(life < 1)
+        {
+            dead = true;
+        }
+    }
+}
